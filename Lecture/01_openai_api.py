ebba59a3ae00959720c5f4edeffaa7fdde47b056
yongheon <jung09965555@gmail.com>
2024-03-15 11:49:12 +0900

    add OpenAI, install langchain

---
+++ Lecture/01_openai_api.py
@@ -0,0 +1,35 @@
+# OPENAI API 사용하기
+# platform.openai.com
+# 1.API - key 발급
+# 2. 카드 등록(VISA, MASTER) + 5.5달러(보유)
+
+
+
+
+# 라이브러리 관리
+# 1.VENV 
+# 2.Anaconda
+
+# python -m venv ./venv
+# .\venv\Scripts\activate
+# pip install
+
+from openai import OpenAI
+client = OpenAI(api_key="")
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "모든 설명을 3줄로 요약해서 설명해주세요"},
+    {"role": "user", "content": "클라우드 설명해줘"}
+  ]
+)
+
+print(completion.choices[0].message)
+
+
+# OpenAI의 API를 사용해서 챗봇 문제점 
+# 1. 개발이 어려움(난이도 상) -> 더 쉽게 개발 할 수 있는 무언가가 필요 -> 프레임워크
+# 2. 챗봇 개발 완성 -> Bard 모델 변경 -> Bard API로 처음부터 개발 -> 프레임워크(LLM)
+
+# -> LangChain프레임워크를 쓰면 코드는 동일한데 모델만 바꾸면 API가 바껴서 실행 
---
+++ README.md
@@ -0,0 +1,14 @@
+# Chosun_ChatGPT
+
+
+
+# Vscode를 사용한 Python 개발환경 구축 
+ 1. VScode 설치
+ 2. Python 설치(python.org) + 환경변수(path) 체크
+ 3. vscode extentions(python, python extension pack, gitgraph or  gitlens) install
+ 4. vscode에서 github 계정으로 로그인(연동)
+ 5. github에서 repository(프로젝트) 생성
+ 6. vscode에서 "git clone repository"로 5번에서 생성한 프로젝트 내려받기
+ 7. 가상환경 생성(venv) : 가상환경 구축 참조
+ 8. vscode에서 python: select interpreter 설정(Ctrl+Shift+P) -> venv
+ 9. pip install langchain
